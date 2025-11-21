0feaaf21b055c26aedf19771478b204215aa8ce1
dhruvilthummar1303 <dhruvilthummar1303@gmail.com>
2025-11-21 15:06:25 +0000

    improved

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import AnimatedBox from './components/AnimatedBox';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React with Angular-like Animations</h1>
+        <AnimatedBox />
+      </header>
+    </div>
+  );
+}
+
+export default App;
